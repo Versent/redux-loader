86a0bff0f4661df147803c5c4001cb233a9430bf
Sebastian Porto <sebasporto@gmail.com>
2015-08-25 16:43:26 +1000

    WIP example

---
+++ example/src/app.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+class Main extends React.Component {
+	render() {
+		return (
+			<div className='container'>
+				APP
+			</div>
+		);
+	}
+}
+
+React.render(
+	<Main />,
+	document.getElementById('app')
+);
